aa8cad81460311a43e3f7b7098e93cfbb4431b52
malathi-maragoni <malathimaragoni@gmail.com>
2026-06-15 21:32:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // 🔌 Connect MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/crm")
+mongoose.connect("mongoose.connect(process.env.MONGO_URI)")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
