b5984eab0cde952ae14f665dcdd9367c0ef6bdad
malathi-maragoni <malathimaragoni@gmail.com>
2026-06-15 21:59:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 // 🔌 Connect MongoDB
-mongoose.connect("mongoose.connect(process.env.MONGO_URI)")
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("MongoDB Error:", err));
 
 // 📦 Model
 const Lead = mongoose.model("Lead", {
